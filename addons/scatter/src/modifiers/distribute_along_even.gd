7895f1e9a87402350a9682ecb4436f842932ee2e
Addmix <addmixtaylor@gmail.com>
2021-01-11 16:57:37 -0700

    imports and scatter update

--- addons/scatter/src/modifiers/distribute_along_even.gd
+++ addons/scatter/src/modifiers/distribute_along_even.gd
@@ -3,9 +3,12 @@ extends "base_modifier.gd"
 
 
 export(float) var interval = 1.0
-export(float) var offset = 0
+export(float, 0, 100) var offset = 0.0
 export(bool) var align_to_path = false
-
+export(int) var align_up_axis = 0
+export(bool) var restrict_x = false
+export(bool) var restrict_y = false
+export(bool) var restrict_z = false
 
 func _init() -> void:
 	display_name = "Distribute Along Path (Even)"
@@ -15,7 +18,8 @@ func _init() -> void:
 func _process_transforms(transforms, _seed) -> void:
 	var path = transforms.path
 	var length: float = path.curve.get_baked_length()
-	var total_count: int = round(length / interval)
+	var total_count: int = round(length / interval)# + int(fmod(length, interval) <= interval / 2)
+	
 	
 	if total_count == 0:
 		warning += """
@@ -23,16 +27,45 @@ func _process_transforms(transforms, _seed) -> void:
 		No transforms could be placed."""
 		return
 	
+	
 	transforms.resize(total_count)
-
 	for i in transforms.list.size():
-		var data = path.get_pos_and_normal(i * interval + offset)
+		var data : Array = path.get_pos_and_normal(fmod(i * interval + abs(offset), length - fmod(length, interval)))
 		var pos: Vector3 = data[0]
 		var normal: Vector3 = data[1]
 		var t : Transform = transforms.list[i]
 		
-		if align_to_path:
-			t = t.rotated(t.basis.y.normalized(), atan2(normal.x, normal.z))
-			
 		t.origin = pos
+		
+		if align_to_path:
+			normal.x *= int(!restrict_x)
+			normal.y *= int(!restrict_y)
+			normal.z *= int(!restrict_z)
+			
+			t = t.looking_at(normal + pos, get_align_up_vector(align_up_axis))
+			
+			
+		
 		transforms.list[i] = t
+
+static func get_align_up_vector(align : int) -> Vector3:
+	var axis : Vector3
+	match align:
+		#x
+		0:
+			print(1)
+			axis = Vector3.RIGHT
+		#y
+		1:
+			print(2)
+			axis = Vector3.UP
+		#z
+		2:
+			print(3)
+			axis = Vector3.BACK
+		_:
+			#default return y axis
+			print(4)
+			axis = Vector3.UP
+	
+	return axis
--- maps/desert storm/Area.gd
+++ maps/desert storm/Area.gd
@@ -3,3 +3,4 @@ extends Area
 func _on_Area_body_entered(body):
 	if body.has_method("damage"):
 		body.damage(self, 100.0)
+
