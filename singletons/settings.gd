151797ea8f12d654c79d2634b7ed965351e57929
Addmix <addmixtaylor@gmail.com>
2021-02-19 17:33:42 -0700

    added sound settings

--- singletons/settings.gd
+++ singletons/settings.gd
@@ -14,4 +14,11 @@ func load_settings() -> void:
 	load_audio()
 
 func load_audio() -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 0)
+	#master volume
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), (1 - max(0, min(1, ProjectSettings.get_setting("audio/volume/master_volume")))) * -80)
+	#sfx volume
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Gunshots"), (1 - max(0, min(1, ProjectSettings.get_setting("audio/volume/sfx_volume")))) * -80)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Explosions"), (1 - max(0, min(1, ProjectSettings.get_setting("audio/volume/sfx_volume")))) * -80)
+	
+	#hitmarkers
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Hitmarkers"), (1 - max(0, min(1, ProjectSettings.get_setting("audio/volume/hitmarker_volume")))) * -80)
