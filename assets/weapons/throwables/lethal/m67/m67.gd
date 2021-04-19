0f92e4fae4b12594725975ad545031f63cd95311
Addmix <addmixtaylor@gmail.com>
2021-04-18 23:21:01 -0600

    start on grenades

---
+++ assets/weapons/throwables/lethal/m67/m67.gd
@@ -0,0 +1,22 @@
+extends RigidBody
+
+onready var timer : Timer = $Timer
+var fragment_count : int = 26
+
+func start() -> void:
+	timer.start()
+
+func _on_Timer_timeout():
+	explode()
+
+func explode() -> void:
+	for i in fragment_count:
+		create_shrapnel(random_vector())
+	
+	queue_free()
+
+func create_shrapnel(dir : Vector3) -> void:
+	pass
+
+func random_vector() -> Vector3:
+	return Vector3(randf(), randf(), randf()).normalized()
