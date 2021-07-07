3bc4f0b5c76efb94abf64e65a8212ebafde2c84c
addmix <addmixtaylor@gmail.com>
2021-07-07 12:01:05 -0600

    fixed naming issue with audio busses

--- singletons/settings.gd
+++ singletons/settings.gd
@@ -60,6 +60,6 @@ func load_audio() -> void:
 	#voice
 	ProjectSettings.set_initial_value("audio/volume/voice_recieve_volume", 1.0)
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("VoiceRecieve"), (1 - max(0, min(1.5, ProjectSettings.get_setting("audio/volume/voice_recieve_volume")))) * -80)
-	ProjectSettings.set_initial_value("audio/volume/voice_transmit_volume", 1.0)
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("VoiceTransmit"), (1 - max(0, min(1.5, ProjectSettings.get_setting("audio/volume/voice_emit_volume")))) * -80)
+	ProjectSettings.set_initial_value("audio/volume/voice_emit_volume", 1.0)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("VoiceEmit"), (1 - max(0, min(1.5, ProjectSettings.get_setting("audio/volume/voice_emit_volume")))) * -80)
 
